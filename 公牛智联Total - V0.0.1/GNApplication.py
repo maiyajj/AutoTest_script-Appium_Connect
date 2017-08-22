34a3e770e29e6c8be887f9503215415176aa5960
maiyajj <1045373828@qq.com>
2017-08-22 14:43:05 +0800

    V0.0.4_17.8.22_2 1、修复启动文件进程循环启动的问题

--- 公牛智联Total - V0.0.1/GNApplication.py
+++ 公牛智联Total - V0.0.1/GNApplication.py
@@ -10,25 +10,36 @@ from src.utils.LaunchAppiumServices import *
 _main_version = ""
 _build_version = ""
 
+
 def run(device_list, device_name):
+    run_appium = 1
+    run_case = 1
     while True:
-        appium = Process(target=LaunchAppiumServices, args=(device_list, device_name,), name=device_name)
-        appium.start()
-        run_case = Process(target=WaitCase, args=(device_list, device_name,))
-        run_case.start()
+        if run_appium == 1:
+            run_appium = 0
+            appium = Process(target=LaunchAppiumServices, args=(device_list, device_name,), name=device_name)
+            appium.start()
+        if run_case == 1:
+            run_case = 0
+            case = Process(target=WaitCase, args=(device_list, device_name,))
+            case.start()
         while True:
             if appium.is_alive() is False:
-                del appium
+                run_appium = 1
+                print appium.pid
                 break
-            elif run_case.is_alive() is False:
-                del run_case
+            elif case.is_alive() is False:
+                run_case = 1
+                print case.pid
                 break
             else:
                 print appium.name
                 time.sleep(1)
+
+
 def check_port(device_list):
     port = {}
-    for k,v in device_list.items():
+    for k, v in device_list.items():
         port[k] = []
         port[k].append(v['port'])
         port[k].append(v['bp_port'])
@@ -37,7 +48,7 @@ def check_port(device_list):
     with open(r"./node_mem_%s.log" % time.strftime("%Y-%m-%d %H:%M:%S"), "w") as files:
         while True:
             try:
-                for k,v in port.items():
+                for k, v in port.items():
                     for i in v:
                         command = 'lsof -i:%s' % i
                         find_pid = re.findall(r"(.+?) .+?(\d+).+LISTEN.+?", os.popen(command).read())
@@ -46,18 +57,23 @@ def check_port(device_list):
                             pid = int(find_pid[0][1])
                             mem = psutil.Process(pid).memory_info().rss / 1024 / 1024
                             p = psutil.virtual_memory()
-                            rest = (p.total * (100 - p.percent)/100)/1024/1024
-                            print "\n***************[%s]|[%s][%s]:[%sM][all_remain:%sM]-----------------[%s]***********************\n" % (i, time.strftime("%Y-%m-%d %H:%M:%S"), proc, mem, rest, k)
-                            files.write("[%s]|[%s][%s]:[%sM][all_remain:%sM]-----------------[%s]\n" % (i, time.strftime("%Y-%m-%d %H:%M:%S"), proc, mem, rest, k))
+                            rest = (p.total * (100 - p.percent) / 100) / 1024 / 1024
+                            print "\n***************[%s]|[%s][%s]:[%sM][all_remain:%sM]-----------------[%s]***********************\n" % (
+                                i, time.strftime("%Y-%m-%d %H:%M:%S"), proc, mem, rest, k)
+                            files.write("[%s]|[%s][%s]:[%sM][all_remain:%sM]-----------------[%s]\n" % (
+                                i, time.strftime("%Y-%m-%d %H:%M:%S"), proc, mem, rest, k))
                 time.sleep(1)
             except BaseException:
                 files.write(traceback.format_exc())
+
+
 def check_proc():
     with open(r"./2.log", "w") as files:
         while True:
             print len(psutil.pids())
             time.sleep(1)
             files.write("\n************%s***************" % len(psutil.pids()))
+
 
 if __name__ == '__main__':
     device_list = AppInit().app_init()
--- 操作练习/守护进程.py
+++ 操作练习/守护进程.py
@@ -7,19 +7,31 @@ def dafe(p):
         print p
         time.sleep(1)
 def run(var):
-    print('.')
+    a = 1
+    b = 1
     while True:
-        appium = multiprocessing.Process(target=dafe, args=("dsf",), name="adfasdf")
-        appium.start()
-        print  appium.pid
+        if a == 1:
+            appium = multiprocessing.Process(target=dafe, args=("dsf",), name="adfasdf")
+            appium.start()
+            print "appium run"
+            a = 0
+        if b == 1:
+            appium1 = multiprocessing.Process(target=dafe, args=("sss",), name="sss")
+            appium1.start()
+            print "appium1 run"
+            b = 0
+        print appium.pid,appium1.pid
         while True:
             if appium.is_alive() is False:
-                # appium.join()
+                a = 1
                 print appium.name, appium.pid
-                # del appium
-                print "appium is death"
-                time.sleep(1)
                 break
+            elif appium1.is_alive() is False:
+                b = 1
+                print appium1.name, appium1.pid
+                break
+            else:
+                time.sleep(1)
 p = multiprocessing.Process(target=run, args=('var',))
 p.start()
 
