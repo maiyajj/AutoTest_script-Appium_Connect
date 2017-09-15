586b213723ee1a642645a3be8b71fc03397c56d7
maiyajj <1045373828@qq.com>
2017-09-15 16:46:38 +0800

    V2.1.3.170915
    1、新增或者删除测试用例后input_case文件修改，手动修改太麻烦

--- 公牛智联Total - V0.0.1/CreatSomeFiles.py
+++ 公牛智联Total - V0.0.1/CreatSomeFiles.py
@@ -1,5 +1,8 @@
 # coding=utf-8
-from src.testcase.case.WaitCase import *
+try:
+    from src.testcase.case.WaitCase import *
+except ImportError:
+    pass
 from src.utils.LaunchAppiumServices import *
 from src.utils.SendMail import *
 
@@ -78,45 +81,73 @@ class CreateFunc(object):
                     files.write("        return self.wrapper(self.pwa.{0}(), self.pwi.{0}())\n".format(i))
 
     def create_INPUT_CASE(self):
+        rootdir = r"./"
+        for parents, dirnames, filenames in os.walk(rootdir):
+            for filename in filenames:
+                if ".DS_Store" in filename:
+                    os.remove(os.path.join(parents, filename))
         # 写INPUT_CASE文件夹内容
         rootdir = r"./src/testcase/case/"  # 指明被遍历的文件夹
-        for parent, dirnames, filenames in os.walk(rootdir):  # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
-            for dirname in [i for i in dirnames if "_" not in i]:
-                if "GNAPP" in dirname:
-                    file_list = []
-                    files_list = {}
-                    file_path = os.path.join(rootdir, "GNAPP/INPUT_CASE")
-                    for parent, dirnames, filenames in os.walk(os.path.join(parent, dirname)):
-                        for dirname in [i for i in dirnames if "INPUT_CASE" not in i]:
-                            path_tmp = os.path.join(parent, dirname)
-                            files = [i for i in os.listdir(path_tmp) if "pyc" not in i and "init" not in i]
-                            input_name = files[0][:-7]
-                            tmp = ""
-                            for i in [i.capitalize() for i in input_name.split("_")[1:]]:
-                                tmp = tmp + i
-                            input_names = "GNApp" + tmp + ".py"
-                            file_list.append(input_names)
-                            files_list[input_names] = files
+        for parents, dirnames, filenames in os.walk(rootdir):  # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
+            for dirname in [i for i in dirnames if "_" not in i and "APP" in i]:
+                name = dirname[:2]
+                app_name = name + "APP"
+                file_list = []
+                files_list = {}
+                file_path = os.path.join(rootdir, "%s/INPUT_CASE" % app_name)
+                for parent, dirnames, filenames in os.walk(os.path.join(parents, dirname)):
+                    for dirname in [i for i in dirnames if "INPUT_CASE" not in i]:
+                        path_tmp = os.path.join(parent, dirname)
+                        files = [i for i in os.listdir(path_tmp) if "pyc" not in i and "init" not in i]
+                        input_name = files[0][:-7]
+                        tmp = ""
+                        for i in [i.capitalize() for i in input_name.split("_")[1:]]:
+                            tmp = tmp + i
+                        input_names = "%sApp" % name + tmp + ".py"
+                        file_list.append(input_names)
+                        files_list[input_names] = files
+                print file_list
+                for k, v in files_list.items():
+                    vv = [i[:-3] for i in v]
+                    with open(os.path.join(file_path, k), "w") as files:
+                        files.write("# coding=utf-8" + "\n")
+                        vv = ["from src.testcase.case.%s.%s.%s import *" % (app_name, i[:-4], i) for i in vv]
+                        for i in vv:
+                            files.write(i + "\n")
+                        files.write("\n")
+                        vv = ["%s%s%s" % (i[:2], "".join([x.capitalize() for x in i[2:-7].split("_")]),
+                                          str(int(i[-6:-3]))) for i in v]
+                        for i in vv:
+                            files.write("{0} = {0}".format(i) + "\n")
 
-                    for k, v in files_list:
-                        print k
-                        print v
-                        # with open(os.path.join(file_path,i), "w") as files:
-                        #     files.write("# coding=utf-8\n")
-                        # with open()
-                        #     for filename in [i for i in filenames if "pyc" not in i and "init" not in i and "App" not in i]:
-                        #         file_list.append(filename)
-                        # print list(set(file_list))
+                with open(os.path.join(file_path, "%sAppInputCase.py" % name), "w") as files:
+                    files.write("# coding=utf-8" + "\n")
+                    for i in [i[:-3] for i in file_list]:
+                        files.write("from src.testcase.case.%s.INPUT_CASE.%s import *" % (app_name, i) + "\n")
+                    files.write("\n")
+                    for i in [i[:-3] + "1" for i in file_list]:
+                        files.write("{0} = {0}".format(i) + "\n")
 
-
-
-                else:
-                    rootpath = os.path.join(parent, dirname)
+    def correct_func_name(self):
+        result = []
+        rootdir = r"./src/testcase/case/"  # 指明被遍历的文件夹
+        for parents, dirnamess, filenamess in os.walk(rootdir):
+            for dirnames in dirnamess:
+                for parent, dirnames, filenames in os.walk(os.path.join(parents, dirnames)):
+                    for filename in [i for i in filenames if "APP" in i and "pyc" not in i]:
+                        with open(os.path.join(parent, filename), "r") as files:
+                            filename = "%s%s%s" % (filename[:2],
+                                                   "".join([i.capitalize() for i in filename[2:-7].split("_")]),
+                                                   str(int(filename[-6:-3])))
+                            result.append(filename == re.findall("class (.+?)\(", files.read())[0])
+        result = list(set(result))
+        print result
 
 
 CF = CreateFunc()
-# CF.create_INPUT_CASE()
-CF.create_AppPageElement(MainPageWidgetAndroidJD, PopupWidgetAndroidJD)
-CF.create_AppPageElement(MainPageWidgetAndroidGN, PopupWidgetAndroidGN)
-CF.create_ReadAPPElement(MainPageWidgetAndroidGN, PopupWidgetAndroidGN)
-CF.create_ReadAPPElement(MainPageWidgetAndroidJD, PopupWidgetAndroidJD)
+CF.create_INPUT_CASE()
+# CF.create_AppPageElement(MainPageWidgetAndroidJD, PopupWidgetAndroidJD)
+# CF.create_AppPageElement(MainPageWidgetAndroidGN, PopupWidgetAndroidGN)
+# CF.create_ReadAPPElement(MainPageWidgetAndroidGN, PopupWidgetAndroidGN)
+# CF.create_ReadAPPElement(MainPageWidgetAndroidJD, PopupWidgetAndroidJD)
+CF.correct_func_name()
--- 公牛智联Total - V0.0.1/src/testcase/case/JDAPP/INPUT_CASE/JDAppInputCase.py
+++ 公牛智联Total - V0.0.1/src/testcase/case/JDAPP/INPUT_CASE/JDAppInputCase.py
@@ -6,5 +6,5 @@ from src.testcase.case.JDAPP.INPUT_CASE.JDAppLogin import *
 
 JDAppAppFunction1 = JDAppAppFunction1
 JDAppCompatibility1 = JDAppCompatibility1
-JDAppLogin1 = JDAppLogin1
 JDAppElectricityMeter1 = JDAppElectricityMeter1
+JDAppLogin1 = JDAppLogin1
